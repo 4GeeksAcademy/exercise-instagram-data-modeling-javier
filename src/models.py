330531da54e2d284b4c1c49bebb4433b0421686c
Javier Gracia <107503117+Javiergracia47@users.noreply.github.com>
2024-12-13 23:22:19 +0000

    proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -5,28 +5,69 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-Base = declarative_base()
+Base = declarative_base() # todas las tablas reiben esta base
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    # aqui definimos las columnas para la tabla personas 
+class User(Base):
+    __tablename__ = 'user'
+    # esta columna recibe como parametro el tipo de datos(integer) que almacenara que es de tipo entero
+    # y le pasamos el primary_key=True)  que hace referencia a que sera nuestra clave primaria 
+    id = Column(Integer , primary_key=True) 
+    # definimos una col username con tipo de dato string y de cantindad de caracteres seran 250max
+    # el nullable nos indica si esa col puede ser o no null y la idea es que no sea vacia entonces es false, si quiero que sea null seria true
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+
+class Post(Base): 
+    __tablename__ = 'post'
+    # aqui definimos las columnas.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id')) # hacemos referencia a la columna de los id de los usuarios 
+    post = relationship(User) # hacemos la conexion con la tabla 6Y75Y7T
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Media(Base):
+      __tablename__ = 'base'
+      id = Column(Integer , primary_key=True)
+      type = Column(String(250),nullable=False)
+      url = Column(String(250),nullable=False)
+      post_id = Column(Integer, ForeignKey('post.id'))
+      media = relationship(Post)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+      __tablename__ = 'comment'
+      id = Column(Integer , primary_key=True)
+      commrnet_text = Column(String(250),nullable=False)
+      author_id = Column(Integer, ForeignKey('user.id'))
+      post_id = Column(Integer, ForeignKey('post.id'))
+      Comment = relationship(User, Post)
+
+
+
+class Like(Base):
+      __tablename__ = 'like'
+      id = Column(Integer , primary_key=True)
+      commrnet_text = Column(String(250),nullable=False)
+      author_id = Column(Integer, ForeignKey('user.id'))
+      post_id = Column(Integer, ForeignKey('post.id'))
+      like = relationship (User, Post)
+
+
+
+class Follow(Base):
+      __tablename__ = 'follow'
+      id =  Column(Integer , primary_key=True)
+      user_from_id = Column(Integer, ForeignKey('user.id'))
+      user_to_id = Column(Integer, ForeignKey('user.id'))
+      like = relationship (User)
+
+
+
+   # def to_dict(self):
+        # return {}
 
 ## Draw from SQLAlchemy base
 try:
